c1fc41902aa8a95f18133570dc12949ce98c4031
Will Robinson <william.ellis.robinson@gmail.com>
2022-11-24 22:09:33 +0000

    more server work

--- server.js
+++ server.js
@@ -4,26 +4,69 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+//NOTE: re add multiple lists so get works as expected
 const lists = [
-    [
-        {id: 1, value: "take out the rubbish", completed: false},
-        {id: 2, value: "take out the garbage", completed: false},
-        {id: 3, value: "walk the dog", completed: false},
-    ],
-    [],
+    {id: 1, value: "take out the rubbish", completed: false},
+    {id: 2, value: "take out the garbage", completed: false},
+    {id: 3, value: "walk the dog", completed: false},
 ];
 
-const returnList = async (id) => {
+const getList = async (id) => {
     return lists[id - 1];
 };
+
+const patchList = async (id, value, completed) => {
+    for (let i = 0; i < lists.length; i++) {
+        if (i.id === id) {
+            i.value = value;
+            i.completed = completed;
+        }
+    }
+};
+
+const postList = async (id, value, completed) => {
+    return null;
+}
 
 app.get("/lists/:id", (request, response) => {
     const id = request.params.id;
 
-    returnList(id)
+    getList(id)
         .then(data => {
             console.log("GET /lists");
-            response.status(200).json({ message: "Completed Successfully!", data: data });
+            response.status(200).json({ message: "List successfully returned!", data: data });
+        })
+        .catch(error => {
+            console.error("Error", error);
+            response.status(500).send("500 Internal Server Error!");
+        });
+});
+
+app.patch("/lists", (request, response) => {
+    const id = request.body.id;
+    const value = requst.body.value;
+    const completed = request.body.value;
+
+    patchList(id)
+        .then(data => {
+            console.log("PATCH /lists");
+            response.status(200).json({ message: "List successfully patched!", data: data });
+        })
+        .catch(error => {
+            console.error("Error", error);
+            response.status(500).send("500 Internal Server Error!");
+        });
+});
+
+app.post("/lists", (request, response) => {
+    const id = request.body.id;
+    const value = requst.body.value;
+    const completed = request.body.value;
+
+    postList(id)
+        .then(data => {
+            console.log("POST /lists");
+            response.status(201).json({ message: "List successfully posted!", data: data });
         })
         .catch(error => {
             console.error("Error", error);
